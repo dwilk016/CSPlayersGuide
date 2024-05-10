74785c7f3d3d927efc1f5f35fdace9ef512851e7
dwilk016 <dwilk016@outlook.com>
2024-05-10 04:23:12 -0400

    Update and rename level2.cs to Level2.cs

---
+++ Level2.cs
@@ -0,0 +1,22 @@
+// Level 2: Getting and IDE
+
+/*
+I will be working on this in two places. At home and At 
+work, as such I will need two different environments to 
+work in that sync. As such I have decieded that I will use
+GITHUB as my note taking platform as I can easily edit and 
+take notes at work and then sync the changes to my computer
+at home. Since the computers are locked down at work I will
+have to use an online compiler to run code. I will use 
+Replit.com if I really need to compile something at work, 
+Otherwise I will wait until I get home.
+*/
+
+/*
+At home I will use Visual Studio for editing and compiling.
+*/
+
+/*
+Challenge: Install Visual Studio
+- [x] Install Visual Studio
+*/
