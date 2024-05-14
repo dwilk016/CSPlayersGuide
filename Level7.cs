56346e217c8297c5f39518f698b2e9ec21b443e8
dwilk016 <dwilk016@outlook.com>
2024-05-14 04:02:50 -0400

    Update Level7.cs

--- Level7.cs
+++ Level7.cs
@@ -81,3 +81,75 @@ int sisterEggs = eggCount / 4;
 Console.WriteLine($"Each sister gets {sisterEggs} and the Duckbear gets {bearEggs}.");
 
 // Ans: 3, 11, and 15 the bear will get more eggs then the sisters. 
+
+/*
+Notes:
+'=' is the assignment operator. 
+
+'a += 5' is the same as a = a + 5
+'a -= 5' is the same as a = a - 5
+'a *= 5'
+'a /= 5'
+'a %= 5'
+
+a++ is a = a + 1
+a-- is a = a - 1
+
+*/
+
+//Challenge: The Dominion of Kings
+
+Console.Write("Number of Estates: ");
+string estateCount = Console.ReadLine();
+Console.Write("Number of Duchy: ");
+string duchyCount = Console.ReadLine();
+Console.Write("Number of Province: ");
+string provinceCount = Console.ReadLine();
+
+int score = Convert.ToInt32(estateCount) + Convert.ToInt32(duchyCount) * 3 + Convert.ToInt32(provinceCount) * 6;
+
+Console.WriteLine($"Your score is {score}.");
+
+
+/*
+Notes:
+you can place the ++ or -- before or after the variable. 
+
+++x will evaluate the increment, then assign it. 
+x++ will assign the variable, then increment it.
+
+int x = 5;
+int y = x++; // result: y = 5
+
+int x = 5;
+int z = ++x; // result: z = 6
+
+# Different Types and Casting
+There are two types of conversion between types. Widening (no lose of information byte => int) and narrowing (lose of information, int => byte).
+Widening is implicit, Narrowing is explicit. 
+
+byte aByte = 3;
+int anInt = aByte;
+
+int anInt = 3;
+byte aByte = (byte) anInt; //this is known as casting and is an explicit conversion.
+
+consider: 
+short a = 2;
+short b = 3;
+int c = a + b; // a and b are converted to ints automatically
+
+short a = 2;
+short b = 3;
+short c = (short)(a + b); // since math is done in ints a + b returns an int and must be cast down. Casting also has a high order of precidence. 
+
+# Math and MathF class
+Math.Pow(x, y) -- x^y
+Math.Sqrt(x) -- sqrt(x)
+Math.Sin(x) -- sin(x) // x is in radians
+Math.Cos(x) -- cos(x)
+Math.Min(x, y) -- returns the smaller
+Math.Max(x, y) -- returns the larger
+Math.Clamp(x, 0, 100) -- returns only 0<x<100
+
+MathF is the same, but with doubles. 
